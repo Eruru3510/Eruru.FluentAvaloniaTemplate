ed87a15b06db15e394d9cc1ba7c8af44a7fd895e
Eruru <1633756198@qq.com>
2026-06-06 13:59:07 +0800

    no changes

--- src/Eruru.FluentAvaloniaTemplate/ViewModels/Pages/HomePageViewModel.cs
+++ src/Eruru.FluentAvaloniaTemplate/ViewModels/Pages/HomePageViewModel.cs
@@ -1,11 +1,8 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-
+#pragma warning disable IDE0130 // 命名空间与文件夹结构不匹配
 namespace Eruru.FluentAvaloniaTemplate.ViewModels {
+#pragma warning restore IDE0130 // 命名空间与文件夹结构不匹配
 
 	public partial class HomePageViewModel : ViewModelBase {
-
-		[ObservableProperty]
-		public partial string Text { get; set; } = string.Empty;
 
 	}
 

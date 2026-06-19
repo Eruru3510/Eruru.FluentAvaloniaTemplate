9da1cd9403c29246b10d7c1b5ed213db0a21049e
Eruru <1633756198@qq.com>
2026-06-19 21:23:13 +0800

    no changes

--- src/Eruru.FluentAvaloniaTemplate/Services/PathService.cs
+++ src/Eruru.FluentAvaloniaTemplate/Services/PathService.cs
@@ -1,8 +1,6 @@
 namespace Eruru.FluentAvaloniaTemplate.Services;
 
-#pragma warning disable CA1052 // 静态容器类型应为 Static 或 NotInheritable
 public class PathService {
-#pragma warning restore CA1052 // 静态容器类型应为 Static 或 NotInheritable
 
 	public static string GetSingleInstanceLockPath (string name) {
 		return Path.Combine (Path.GetTempPath (), nameof (Eruru), Api.AppId, $"{name}.lock");

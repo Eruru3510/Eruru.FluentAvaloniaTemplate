2b5ed159b08b62788151f9cfacf72a0633883c63
Eruru <1633756198@qq.com>
2026-06-21 01:04:14 +0800

    update FluentAvaloniaUI nuget package

--- src/Eruru.FluentAvaloniaTemplate/ViewModels/Navigation/NavigationItemViewModel.cs
+++ src/Eruru.FluentAvaloniaTemplate/ViewModels/Navigation/NavigationItemViewModel.cs
@@ -1,11 +1,15 @@
 using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
 using CommunityToolkit.Mvvm.ComponentModel;
 using FluentAvalonia.UI.Controls;
 
 namespace Eruru.FluentAvaloniaTemplate.ViewModels;
 
 public partial class NavigationItemViewModel (
-	string content, Type viewType, Func<object?, object?>? getDataContext, object? state = null,
+	string content,
+	[DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]
+	Type viewType,
+	Func<object?, object?>? getDataContext, object? state = null,
 	FAIconSource? iconSource = null
 ) : ViewModelBase {
 
@@ -17,6 +21,7 @@ public partial class NavigationItemViewModel (
 	public partial object? DataContext { get; set; }
 	[ObservableProperty]
 	public partial ObservableCollection<NavigationItemViewModel> Items { get; set; } = [];
+	[DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]
 	public Type ViewType { get; } = viewType;
 	public NavigationItemViewModel? Parent { get; set; }
 
